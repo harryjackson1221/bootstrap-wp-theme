64d2339429990c8e70672a3b2671d848a320ea71
harryjackson1221 <harryjackson1221@gmail.com>
2017-11-23 10:53:49 -0500

    updated theme css

--- index.php
+++ index.php
@@ -37,13 +37,13 @@
 <div class="section">
 <div class="container-fluid footer-widgets">
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-4 col-sm-12">
 <?php dynamic_sidebar('footer-1'); ?>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 col-sm-6">
 <?php dynamic_sidebar('footer-2'); ?>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 col-sm-6">
 <?php dynamic_sidebar('footer-3'); ?>
     </div>
   </div>
