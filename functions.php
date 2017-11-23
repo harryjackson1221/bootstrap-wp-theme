adbaaa2d8691df9266651b49cc063e2207dc3a05
harryjackson1221 <harryjackson1221@gmail.com>
2017-11-23 09:12:06 -0500

    moved assets to inc/

--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@ function wp_bootstrap_enqueue_assets() {
 	//wp_deregister_script ('jquery');
 
 	//Bootstrap core CSS
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/inc/vendor/bootstrap/css/bootstrap.min.css' );
 
 	//Custom fonts for this template 
-	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/vendor/font-awesome/css/font-awesome.min.css' );
-	wp_enqueue_style( 'simple-line-icons', get_stylesheet_directory_uri() . '/vendor/simple-line-icons/css/simple-line-icons.css' );
+	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/inc/vendor/font-awesome/css/font-awesome.min.css' );
+	wp_enqueue_style( 'simple-line-icons', get_stylesheet_directory_uri() . '/inc/vendor/simple-line-icons/css/simple-line-icons.css' );
 
 
     //Google Fonts 
@@ -19,21 +19,21 @@ function wp_bootstrap_enqueue_assets() {
     
 
     //Custom styles for this template
-    wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/css/new-age.css' );
+    wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/inc/css/new-age.css' );
 
 
 	//Enqueue the JavaScript stuff
-	wp_enqueue_script( 'theme-jquery', get_stylesheet_directory_uri() . '/vendor/jquery/jquery.js', array(), '3.2.1', true );
-	wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . '/vendor/bootstrap/js/bootstrap.bundle.min.js', array(), '4.0.0', true );
-	wp_enqueue_script( 'jquery-easing', get_stylesheet_directory_uri() . '/vendor/jquery-easing/jquery.easing.min.js', array(), '1.4.1', true );
-	wp_enqueue_script( 'theme-js', get_stylesheet_directory_uri() . '/js/new-age.js', array('jquery-easing'), '1.4.1', true );
+	wp_enqueue_script( 'theme-jquery', get_stylesheet_directory_uri() . '/inc/vendor/jquery/jquery.js', array(), '3.2.1', true );
+	wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . '/inc/vendor/bootstrap/js/bootstrap.bundle.min.js', array(), '4.0.0', true );
+	wp_enqueue_script( 'jquery-easing', get_stylesheet_directory_uri() . '/inc/vendor/jquery-easing/jquery.easing.min.js', array(), '1.4.1', true );
+	wp_enqueue_script( 'theme-js', get_stylesheet_directory_uri() . '/inc/js/new-age.js', array('jquery-easing'), '1.4.1', true );
 
 }
 
 add_action( 'wp_enqueue_scripts' , 'wp_bootstrap_enqueue_assets' );
 
 // Add the Bootstrap Nav Walker
-require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
+require_once get_template_directory() . '/inc/wp-bootstrap-navwalker.php';
 
 // Make sure that the primary-menu and footer-menu have the correct classes in them
 function primary_add_menu_atts( $atts, $item, $args ) {
