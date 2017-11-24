31e7e2ad64fbd0f6c5378b96040213528c63cf20
harryjackson1221 <harryjackson1221@gmail.com>
2017-11-24 13:47:18 -0500

    adjusted css

--- index.php
+++ index.php
@@ -34,16 +34,16 @@
       </div>
     </div>
   </div>
-<div class="section">
-<div class="container-fluid footer-widgets">
+<div id="footer-widgets" class="section">
+<div class="container footer-widgets">
   <div class="row">
     <div class="col-md-4 col-sm-12">
 <?php dynamic_sidebar('footer-1'); ?>
     </div>
-    <div class="col-md-4 col-sm-6">
+    <div class="col-md-4 col-sm-12">
 <?php dynamic_sidebar('footer-2'); ?>
     </div>
-    <div class="col-md-4 col-sm-6">
+    <div class="col-md-4 col-sm-12">
 <?php dynamic_sidebar('footer-3'); ?>
     </div>
   </div>
